0d31d1c1e808fd628160d281acc475b079ed8e00
Soumit-Das <soumitdas500@gmail.com>
2024-08-18 14:09:39 +0530

    Basic_Maths : Reverse Integer

---
+++ DSA_Java/Basic_Maths/src/Reverse_Integer.java
@@ -0,0 +1,43 @@
+/*
+Question
+
+Given a signed 32-bit integer x, return x with its digits reversed.
+If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
+
+Example 1:
+
+Input: x = 123
+Output: 321
+Example 2:
+
+Input: x = -123
+Output: -321
+Example 3:
+
+Input: x = 120
+Output: 21
+
+Link : https://leetcode.com/problems/reverse-integer/description/
+
+*/
+
+
+public class Reverse_Integer {
+    public static int reverse(int x) {
+        int rev = 0;
+        int lastDigit = 0;
+        while (x != 0) {
+            lastDigit = x % 10;
+            if (rev > Integer.MAX_VALUE / 10 || rev < Integer.MIN_VALUE / 10) {
+                return 0;
+            }
+            rev = (rev * 10) + lastDigit;
+            x = x / 10;
+        }
+        return rev;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(reverse(-132));
+    }
+}
