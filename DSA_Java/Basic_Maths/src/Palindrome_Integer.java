76752be4c180c915aa0a9cfff70c81aabc41370e
Soumit-Das <soumitdas500@gmail.com>
2024-08-19 08:38:57 +0530

    Basic Maths : Palindrome Integer

---
+++ DSA_Java/Basic_Maths/src/Palindrome_Integer.java
@@ -0,0 +1,22 @@
+public class Palindrome_Integer {
+    public static boolean palindrome(int x){
+        if(x < 0){
+            return false;
+        }
+        int dup = x;
+        int rev = 0;
+        int last = 0;
+        while(x != 0){
+            last = x % 10;
+            rev = (rev * 10) + last;
+            x = x / 10;
+        }
+        if(rev == dup){
+            return true;
+        }
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println(palindrome(-121));
+    }
+}
