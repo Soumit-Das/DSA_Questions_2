3436051413400b44fcd07f5ad77c3efe188cd352
Soumit-Das <soumitdas500@gmail.com>
2024-08-27 22:06:17 +0530

    Basic_Recursion : added comments

--- DSA_Java/Basic_Recurssion/src/Print_N_to_1.java
+++ DSA_Java/Basic_Recurssion/src/Print_N_to_1.java
@@ -1,3 +1,19 @@
+/*
+
+Print numbers from N to 1 (space separated) without the help of loops.
+
+Example 1:
+
+Input:
+N = 10
+Output: 10 9 8 7 6 5 4 3 2 1
+
+Your Task:
+This is a function problem. You only need to complete the function printNos() that takes N as parameter and prints number from N to 1
+recursively.
+
+ */
+
 public class Print_N_to_1 {
     public static void printReverse(int n){
         if(n < 1){
