4e4759e3f805af2ab3951c4935f918d5815a46e7
Soumit-Das <soumitdas500@gmail.com>
2025-07-27 10:38:37 +0530

    added comments in the Prime range question

--- DSA_Java/Number_Theory/src/Prime_Number_In_a_Range.java
+++ DSA_Java/Number_Theory/src/Prime_Number_In_a_Range.java
@@ -1,5 +1,41 @@
 import java.util.ArrayList;
 
+/*
+There we will be using Prime Sieve or Sieve of Erastosthens method which is an optimised method to solve ths question
+
+Question print all prime numbers in a range
+
+Sieve of Erastosthens says that if x is a prime number then all the factors of it will be non-prime number
+
+Code approach
+
+1. First, we create a boolean array of size (N + 1).
+   → Why? Because we want to check and mark all numbers from 0 to N, whether they are prime or not.
+
+2. We mark all values from 2 to N as `true`.
+   → Why? Because initially we assume all numbers are prime. We will later mark non-primes as false.
+
+3. We set 0 and 1 as `false`.
+   → Why? Because 0 and 1 are not prime numbers by definition.
+
+4. Now we run a loop from 2 to N:
+   - For each number `i`, if it's still marked `true`, that means it's a prime.
+   - Then we mark all multiples of `i` (starting from 2 * i) as `false`.
+   → Why? Because multiples of a prime number cannot be prime. For example, if 2 is prime, then 4, 6, 8... are not prime.
+
+5. After this process, the boolean array tells us which numbers are prime (true) and which are not (false).
+
+6. Now we create a list to store the prime numbers in the range M to N:
+   - We check each number from M to N in the boolean array.
+   - If it's `true`, we add it to the result list.
+   → Why? Because we only want to return the primes in the specified range.
+
+7. Finally, return this list and print it.
+
+This approach is fast and efficient compared to checking each number one-by-one using normal logic.
+ */
+
+
 public class Prime_Number_In_a_Range {
 
     ArrayList<Integer> primeRange(int M, int N) {
